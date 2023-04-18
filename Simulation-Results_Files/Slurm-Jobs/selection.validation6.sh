d05266b3c32bbc9ec268eb39ee627e53ad233d03
Shashank Pritam <shashankpritam@outlook.com>
2023-04-18 18:52:04 -0500

    Update selection.validation6.sh

--- Simulation-Results_Files/Slurm-Jobs/selection.validation6.sh
+++ Simulation-Results_Files/Slurm-Jobs/selection.validation6.sh
@@ -12,15 +12,6 @@ folder="Simulation-Results/Insertion-Bias/validation_6"
 genome="mb:1"
 rep=100
 
-# Updated base population without insertion bias
-echo "5000 R 0; 0;
-2500 R 0; 0; 0;
-2500 R 0;;" > $folder/input_sel
-
-# Updated base population without insertion bias
-echo "5000 R 0; 0;
-2500 R 0; 0; 0;
-2500 R 0;;" > $folder/input_sel
 
 
 # All commands updated
@@ -28,15 +19,15 @@ $tool --N 1000 --gen 100 --genome $genome --cluster kb:100 --rr 4 --rep $rep --u
 
 $tool --N 1000 --gen 100 --genome $genome --cluster kb:100 --rr 4 --rep $rep --u 0.1 --basepop "100(0)" --steps 25 -x 0.1 -no-x-cluins --file-mhp $folder/validation_6_2_mhp > $folder/validation_6_2 &
 
-$tool --N 10000 --u 0 --basepop $folder/input_sel --gen 1000 --genome $genome --steps 10 --rr 0 --rep $rep --sampleid psel3 > $folder/validation_6_3 &
+$tool --N 10000 --u 0 --basepop "10000(0)" --gen 1000 --genome $genome --steps 10 --rr 0 --rep $rep --sampleid psel3 > $folder/validation_6_3 &
 
-$tool --N 10000 --u 0 -x 0.1 --basepop $folder/input_sel --gen 1000 --genome $genome --steps 10 --rr 0 --rep $rep --sampleid psel4 > $folder/validation_6_4 &
+$tool --N 10000 --u 0 -x 0.1 --basepop "10000(0)" --gen 1000 --genome $genome --steps 10 --rr 0 --rep $rep --sampleid psel4 > $folder/validation_6_4 &
 
-$tool --N 10000 --u 0 -x 0.01 --basepop $folder/input_sel --gen 1000 --genome $genome --steps 10 --rr 0 --rep $rep --sampleid psel5 > $folder/validation_6_5 &
+$tool --N 10000 --u 0 -x 0.01 --basepop "10000(0)" --gen 1000 --genome $genome --steps 10 --rr 0 --rep $rep --sampleid psel5 > $folder/validation_6_5 &
 
-$tool --N 10000 --u 0 -x 0.001 --basepop $folder/input_sel --gen 1000 --genome $genome --steps 10 --rr 0 --rep $rep --sampleid psel6 > $folder/validation_6_6 &
+$tool --N 10000 --u 0 -x 0.001 --basepop "10000(0)" --gen 1000 --genome $genome --steps 10 --rr 0 --rep $rep --sampleid psel6 > $folder/validation_6_6 &
 
-$tool --N 10000 --u 0 -x 0.0001 --basepop $folder/input_sel --gen 1000 --genome $genome --steps 10 --rr 0 --rep $rep --sampleid psel7 > $folder/validation_6_7 &
+$tool --N 10000 --u 0 -x 0.0001 --basepop "10000(0)" --gen 1000 --genome $genome --steps 10 --rr 0 --rep $rep --sampleid psel7 > $folder/validation_6_7 &
 
 # Wait for all simulations to finish
 wait
