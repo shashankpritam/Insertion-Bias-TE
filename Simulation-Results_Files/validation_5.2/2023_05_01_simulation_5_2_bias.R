41f5d1d9a42032a42e21c9937cc1944a60ce8808
Shashank Pritam <shashankpritam@outlook.com>
2023-05-18 14:02:28 -0500

    more figs

--- Simulation-Results_Files/validation_5.2/2023_05_01_simulation_5_2_bias.R
+++ Simulation-Results_Files/validation_5.2/2023_05_01_simulation_5_2_bias.R
@@ -49,6 +49,49 @@ g <- ggplot() +
 plot(g)
 
 
+# Plotting 90, 70, 0, -70, -80 & -90 values of Bias Only
+# library(tidyverse)
+# library(RColorBrewer)
+# library(ggpubr)
+# theme_set(theme_bw())
+# 
+# p <- c("#1a9850", "#ffd700", "#d73027")
+# 
+# df <- read.table("2023_05_01_simulation_5_2_bias", fill = TRUE, sep = "\t")
+# names(df) <- c("rep", "gen", "popstat", "spacer_1", "fwte", "avw", "min_w", "avtes", "avpopfreq",
+#                "fixed", "spacer_2", "phase", "fwcli", "avcli", "fixcli", "spacer_3",
+#                "avbias", "3tot", "3cluster", "spacer_4", "sampleid")
+# df <- df[, !is.na(colnames(df))]
+# 
+# df$phase <- factor(df$phase, levels = c("rapi", "trig", "shot", "inac"))
+# df <- df %>% filter(sampleid %in% c("b90", "b70", "b0", "bm70", "bm80", "bm90"))
+# df$sampleid <- factor(df$sampleid, levels = c("b90", "b70", "b0", "bm70", "bm80", "bm90"))
+# 
+# g <- ggplot() +
+#   geom_line(data = df, aes(x = gen, y = avtes, group = rep, color = phase), alpha = 1, linewidth = 0.7) +
+#   xlab("Generation") +
+#   ylab("TEs insertions per diploid individual") +
+#   theme(legend.position = "bottom",
+#         plot.title = element_text(hjust = 0.5),
+#         panel.grid.major = element_line(colour = "gray90"),
+#         panel.grid.minor = element_line(colour = "gray95"),
+#         strip.background = element_rect(fill = "lightgrey"),
+#         strip.text = element_text(face = "bold")) +
+#   ggtitle("Bias vs TE Insertions") +
+#   scale_colour_manual(values = p) +
+#   facet_wrap(~sampleid, labeller = labeller(sampleid =
+#                                               c("b90" = "bias = 90",
+#                                                 "b70" = "bias = 70",
+#                                                 "b0" = "bias = 0",
+#                                                 "bm70" = "bias = -70",
+#                                                 "bm80" = "bias = -80",
+#                                                 "bm90" = "bias = -90")))
+# 
+# plot(g)
+
+
+
+
 # Divide in shot and inact phases
 df1 <- subset(df, phase %in% c("shot", "inac"))
 df2 <- data.frame()
@@ -138,6 +181,88 @@ g_avtes <- ggplot(df_summary, aes(x = phase, y = av_tes, fill = phase)) +
 plot(g_avtes)
 
 
+
+# # Remove the extra NA column
+# df <- df[, !is.na(colnames(df))]
+# # Filter out the desired bias values
+# df <- df %>% filter(sampleid %in% c("b90", "b70", "b0", "bm70", "bm80", "bm90"))
+# 
+# # Divide in shot and inact phases
+# df1 <- subset(df, phase %in% c("shot", "inac"))
+# df2 <- data.frame()
+# 
+# # New dataframe with only the first shotgun & the first inactive phase of each replicate
+# repcheck <- 1
+# x <- 1
+# y <- 1
+# while (x < nrow(df1)) {
+#   if (repcheck != df1[x, 1]) {
+#     y <- 1
+#   }
+#   if (y == 1) {
+#     if (df1[x, "phase"] == "shot") {
+#       df2 <- rbind(df2, df1[x,])
+#       y <- 2
+#       repcheck <- df1[x, 1]
+#     }
+#   }
+#   if (y == 2) {
+#     if (df1[x, "phase"] == "inac") {
+#       df2 <- rbind(df2, df1[x,])
+#       y <- 1
+#     }
+#   }
+#   x <- x + 1
+# }
+# 
+# # Summary statistics
+# df2 <- select(df2, -c("spacer_1", "spacer_2", "spacer_3", "spacer_4"))
+# 
+# df_count <- df2 %>%
+#   dplyr::count(sampleid, phase)
+# 
+# df_summary <- df2 %>%
+#   dplyr::group_by(sampleid, phase) %>%
+#   dplyr::summarize(av_fwcli = mean(fwcli), sd_fwcli = sd(fwcli),
+#                    av_cli = mean(avcli), sd_cli = sd(avcli), cv_cli_percent = sd(avcli) / mean(avcli),
+#                    av_tes = mean(avtes), sd_tes = sd(avtes), cv_tes_percent = sd(avtes) / mean(avtes),
+#                    length_previous_phase = mean(gen),
+#                    sd_gen_phases = sd(gen))
+# df_summary <- cbind(df_count$n, df_summary)
+# 
+# colnames(df_summary)[1] <- "n"
+# 
+# # CI 95%: z* sd/sqrt(population)
+# df_summary$ci_fwcli <- qt(0.975, df = df_summary$n - 1) * (df_summary$sd_fwcli / sqrt(df_summary$n))
+# df_summary$ci_cli <- qt(0.975, df = df_summary$n - 1) * (df_summary$sd_cli / sqrt(df_summary$n))
+# df_summary$ci_tes <- qt(0.975, df = df_summary$n - 1) * (df_summary$sd_tes / sqrt(df_summary$n))
+# 
+# # Average TE insertions per individual shot and inac phases
+# g_avtes <- ggplot(df_summary, aes(x = phase, y = av_tes, fill = phase)) +
+#   geom_bar(stat = "identity") +
+#   geom_errorbar(aes(x = phase, ymin = av_tes - sd_tes, ymax = av_tes + sd_tes), width = 0.2, colour = "black", alpha = 0.9, size = 0.8) +
+#   ylab("Insertions per individual") +
+#   xlab("Phase") +
+#   theme(legend.position = "bottom",
+#         plot.title = element_text(hjust = 0.5),
+#         panel.grid.major = element_line(colour = "gray90"),
+#         panel.grid.minor = element_line(colour = "gray95"),
+#         strip.background = element_rect(fill = "lightgrey"),
+#         strip.text = element_text(face = "bold")) +
+#   ggtitle("Bias vs TE Insertions") +
+#   scale_y_continuous(expand = expansion(mult = c(0, 0.01))) +
+#   scale_fill_manual(values = c("#ffd700", "#d73027")) +
+#   facet_wrap(~sampleid, labeller = labeller(sampleid =
+#                                               c("b0" = "bias = 0",
+#                                                 "b70" = "bias = 70",
+#                                                 "b90" = "bias = 90",
+#                                                 "bm70" = "bias = -70",
+#                                                 "bm80" = "bias = -80",
+#                                                 "bm90" = "bias = -90")))
+# 
+# plot(g_avtes)
+                
+
 # Average cluster insertions per individual shot and inac phases
 g_avcli <- ggplot(df_summary, aes(x = phase, y = av_cli, fill = phase)) +
   geom_bar(stat = "identity") +
@@ -175,6 +300,32 @@ g_avcli <- ggplot(df_summary, aes(x = phase, y = av_cli, fill = phase)) +
                                                 "bm90" = "bias = -90")))
 
 plot(g_avcli)
+
+# g_avcli <- ggplot(df_summary, aes(x = phase, y = av_cli, fill = phase)) +
+#   geom_bar(stat = "identity") +
+#   geom_errorbar(aes(x = phase, ymin = av_cli - sd_cli, ymax = av_cli + sd_cli), width = 0.2, colour = "black", alpha = 0.9, size = 0.8) +
+#   ylab("Cluster insertions per individual") +
+#   xlab("Phase") +
+#   theme(legend.position = "bottom",
+#         plot.title = element_text(hjust = 0.5),
+#         panel.grid.major = element_line(colour = "gray90"),
+#         panel.grid.minor = element_line(colour = "gray95"),
+#         strip.background = element_rect(fill = "lightgrey"),
+#         strip.text = element_text(face = "bold")) +
+#   ggtitle("Bias vs Cluster Insertions") +
+#   scale_y_continuous(expand = expansion(mult = c(0, 0.01))) +
+#   scale_fill_manual(values = c("#ffd700", "#d73027")) +
+#   facet_wrap(~sampleid, labeller = labeller(sampleid =
+#                                               c("b0" = "bias = 0",
+#                                                 "b70" = "bias = 70",
+#                                                 "b90" = "bias = 90",
+#                                                 "bm70" = "bias = -70",
+#                                                 "bm80" = "bias = -80",
+#                                                 "bm90" = "bias = -90")))
+# 
+# plot(g_avcli)
+
+
 
 
 g_bar_av_TEs <- ggplot(df2, aes(x = sampleid, y = avtes)) +
