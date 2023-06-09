549c9b0c91224f7e763ada7ca3656d5ef95ddbb6
Shashank Pritam <shashankpritam@outlook.com>
2023-06-09 15:57:02 -0500

    bias sim job written

--- Simulation-Results_Files/Slurm-Jobs/insertion.validation7.sh
+++ Simulation-Results_Files/Slurm-Jobs/insertion.validation7.sh
@@ -7,8 +7,53 @@
 #SBATCH --error=job.%A.err
 #SBATCH --output=job.%A.out
 
-
-
+# Base name for the output files
+outfile_base="input_bias"
 
 tool="./main"
+genome="mb:1"
+cluster="kb:30"
+rep=100
+gen=1
+steps=1
 folder="Simulation-Results/Insertion-Bias/validation_7"
+
+# Make sure the output folder exists
+mkdir -p $folder
+
+# Initialize a counter
+counter=1
+
+# Loop over values from -100 to 100 in steps of 10
+for j in $(seq -100 10 100)
+do
+    # Formulate the outfile name for this iteration
+    outfile="${outfile_base}${counter}"
+
+    # Remove the outfile if it already exists
+    if [ -e "$outfile" ]
+    then
+        rm "$outfile"
+    fi
+
+    # Start the line with "10000;" and the first random number
+    line="10000; $(( RANDOM % 10000001 ))(${j})"
+
+    # Add 999 more random numbers and the bias value to the line
+    for i in {1..999}
+    do
+        line="$line, $(( RANDOM % 10000001 ))(${j})"
+    done
+
+    # Finish the line with "; 0(0)" and write it to the file
+    echo "$line; 0(0)" >> $outfile
+
+    # Specify basepop with the current outfile
+    basepop="file:$outfile"
+
+    # Run the command
+    $tool --N 10000 --gen $gen --genome $genome --cluster $cluster --rep $rep --u 0.1 --basepop $basepop --steps $steps >> "$folder/result_${counter}.out"
+
+    # Increment the counter
+    counter=$((counter+1))
+done
