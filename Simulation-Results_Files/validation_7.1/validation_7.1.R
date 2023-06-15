a94244a25cf8c38077eede790dd40122104cef34
Shashank Pritam <shashankpritam@outlook.com>
2023-06-15 14:44:10 -0500

    7.1

--- Simulation-Results_Files/validation_7.1/validation_7.1.R
+++ Simulation-Results_Files/validation_7.1/validation_7.1.R
@@ -1,24 +1,46 @@
-# Load necessary libraries
+---
+title: "Bias Validation"
+output: html_notebook
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
 library(ggplot2)
 library(readr)
 library(dplyr)
 library(stringr)
 library(purrr)
 library(tidyr)
+```
 
+# Introduction
 
+This notebook illustrates the data processing and visualization steps. We start by loading necessary libraries, defining some parameters, and then we process the data files. The final part of the notebook presents a bar plot of `avtes` for 21 `sampleid`s.
 
-# Initialize an empty dataframe to store results
-result_df <- data.frame()
+# Data Processing
+
+This part includes loading and cleaning the data. First, we specify the column names for our data. Then, we define a function `process_file` to read and process each file. 
+
+```{r data-processing, message=FALSE, warning=FALSE}
+# Define column names
+column_names <- c("rep", "gen", "popstat", "spacer_1", "fwte", "avw", "min_w", "avtes", "avpopfreq",
+                  "fixed","spacer_2", "phase", "fwcli", "avcli", "fixcli", "spacer_3",
+                  "avbias", "3tot", "3cluster", "spacer_4", "sampleid")
+
+# Define numeric columns
+numeric_columns <- c("rep", "gen", "fwte", "avw", "min_w", "avtes", "avpopfreq",
+                     "fixed", "fwcli", "avcli", "fixcli",
+                     "avbias")
 
 # Define N as the total number of result files
 N <- 21
 
-# Loop over result files
-for(i in seq(1, N)) {
+# Create a function to process each file
+process_file <- function(i) {
+  # Construct filename
   filename <- paste0("result_", i, ".out")
   
-  # Read the result file
+  # Read and process the result file
   result <- read_lines(filename) %>% 
     discard(~str_detect(., "^#|^Invade")) %>%  # Remove lines starting with '#' or 'Invade'
     str_split("\\s+", simplify = TRUE) %>%  # Split lines into columns by whitespace
@@ -28,14 +50,36 @@ for(i in seq(1, N)) {
   result <- result[,-ncol(result)]
   
   # Set column names
-  colnames(result) <-c("rep", "gen", "popstat", "spacer_1", "fwte", "avw", "min_w", "avtes", "avpopfreq",
-                       "fixed","spacer_2", "phase", "fwcli", "avcli", "fixcli", "spacer_3",
-                       "avbias",	"3tot",	"3cluster",	"spacer 4", "sampleid")
+  colnames(result) <- column_names
   
-  # Convert columns to appropriate data types
-  result <- mutate(result, across(everything(), as.numeric), .keep = "all")
+  # Convert numeric columns to appropriate data types
+  result <- mutate(result, across(all_of(numeric_columns), as.numeric), .keep = "all")
   
-  # Append result to result_df
-  result_df <- rbind(result_df, result)
+  return(result)
 }
+
+# Loop over result files and combine them into a single data frame
+result_df <- map_df(seq_len(N), process_file)
+```
+
+# Data Visualization
+
+After the data is cleaned, we create a barplot of `avtes` for 21 `sampleid`s.
+
+```{r bias_validation, message=FALSE, warning=FALSE}
+ggplot(result_df, aes(x=factor(sampleid), y=avtes)) + 
+  geom_bar(stat='identity') + 
+  labs(title = "Barplot of 'avtes' for 21 SampleIDs", x = "Sample ID", y = "avtes") +
+  theme_minimal()
+```
+This bar plot gives us insights about ...
+
+# Conclusion
+
+The notebook illustrated how we can efficiently process our data files and generate insightful visualizations. The bar plot gives us an understanding of ...
+
+```{r session-info, echo=FALSE}
+sessionInfo()
+```
+
 
