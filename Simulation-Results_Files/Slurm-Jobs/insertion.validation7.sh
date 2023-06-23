3f1de280db7e98b90637e8d036bd8efca7a6b297
Shashank Pritam <shashankpritam@outlook.com>
2023-06-23 10:22:13 -0500

    some heavy filteration

--- Simulation-Results_Files/Slurm-Jobs/insertion.validation7.sh
+++ Simulation-Results_Files/Slurm-Jobs/insertion.validation7.sh
@@ -7,8 +7,7 @@
 #SBATCH --error=job.%A.err
 #SBATCH --output=job.%A.out
 
-# Base name for the output files
-outfile_base="input_bias"
+#!/bin/bash
 
 tool="./main"
 genome="mb:1"
@@ -22,44 +21,20 @@ rr=0
 # Make sure the output folder exists
 mkdir -p $folder
 
-# Initialize a counter
-counter=1
-
-# Function to generate a random number between 0 and 1 million
-generate_random_number() {
-    echo $(( $(od -An -N3 -i /dev/urandom) % 1000001 ))
-}
-
 # Loop over values from -100 to 100 in steps of 10
 for j in $(seq -100 10 100)
 do
-    # Formulate the outfile name for this iteration
-    outfile="$folder/${outfile_base}${counter}"  # Added $folder to the path
+    # Set basepop directly to "1000;j"
+    basepop="1000($j)"
 
-    # Remove the outfile if it already exists
-    if [ -e "$outfile" ]
+    # Assign current counter value to sampleid with descriptive prefix
+    if [ $j -ge 0 ]
     then
-        rm "$outfile"
+        sampleid="b${j}"
+    else
+        sampleid="mb${j#-}"  # Use parameter expansion to remove the negative sign
     fi
 
-    # Start the line with "10000;" and the first random number
-    line="10000; $(generate_random_number)(${j})"
-
-    # Add 999 more random numbers and the bias value to the line
-    for i in {1..999}
-    do
-        line="$line, $(generate_random_number)(${j})"
-    done
-
-    # Finish the line with "; 0(0)" and write it to the file
-    echo "$line; 0(0)" >> $outfile
-
-    # Specify basepop with the current outfile
-    basepop="file:$outfile"  # Now correctly pointing to the outfile in $folder
-
     # Run the command
-    $tool --N 10000 --gen $gen --genome $genome --cluster $cluster --rr $rr --rep $rep --u 0.1 --basepop $basepop --steps $steps >> "$folder/result_${counter}.out"
-
-    # Increment the counter
-    counter=$((counter+1))
+    $tool --N 100000 --gen $gen --genome $genome --cluster $cluster --rr $rr --rep $rep --basepop "$basepop" --steps $steps --sampleid $sampleid >> "$folder/result_${sampleid}.out"
 done
