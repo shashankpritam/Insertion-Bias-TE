e381ec8d33453cb61ab5bddd3996f50e2872d2a2
Shashank Pritam <shashankpritam@outlook.com>
2023-06-23 10:59:22 -0500

    ups

--- Simulation-Results_Files/Slurm-Jobs/insertion.validation7.sh
+++ Simulation-Results_Files/Slurm-Jobs/insertion.validation7.sh
@@ -7,16 +7,15 @@
 #SBATCH --error=job.%A.err
 #SBATCH --output=job.%A.out
 
-#!/bin/bash
 
 tool="./main"
-genome="mb:1"
-cluster="kb:30"
-rep=100
+genome="mb:1,1,1,1,1"
+cluster="kb:30,30,30,30,30"
+rep=1
 gen=1
 steps=1
 folder="Simulation-Results/Insertion-Bias/validation_7"
-rr=0
+rr="0,0,0,0,0"
 
 # Make sure the output folder exists
 mkdir -p $folder
@@ -35,6 +34,7 @@ do
         sampleid="mb${j#-}"  # Use parameter expansion to remove the negative sign
     fi
 
-    # Run the command
-    $tool --N 100000 --gen $gen --genome $genome --cluster $cluster --rr $rr --rep $rep --basepop "$basepop" --steps $steps --sampleid $sampleid >> "$folder/result_${sampleid}.out"
+    # Run the command and write the output to a file named after the sampleid
+    $tool --N 100000 --gen $gen --genome $genome --cluster $cluster --rr $rr --rep $rep --basepop "$basepop" --steps $steps --sampleid $sampleid > "$folder/result_${sampleid}.out"
 done
+
