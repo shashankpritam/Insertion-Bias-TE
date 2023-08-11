40bf7f6ae64482a2ec2f7f19ee45097513cfc0f8
Shashank Pritam <shashankpritam@outlook.com>
2023-08-11 12:48:18 -0500

    updates py

--- Simulation-Results_Files/simulation_storm/sim_storm.py
+++ Simulation-Results_Files/simulation_storm/sim_storm.py
@@ -43,9 +43,40 @@ def current_milli_time():
 def get_rand_bias():
     return random.randint(-100, 100)
 
+
+# The default directory is dynamic and depends on the time this scipt is invoked
+def get_default_output_directory():
+    current_time = time.strftime("%dth%b%yat%I%M%S%p", time.gmtime())
+    default_output_directory = os.path.join("/Users", "shashankpritam", "github", "Insertion-Bias-TE", "Simulation-Results_Files", "simulation_storm", current_time)
+
+    if not os.path.exists(default_output_directory):
+        os.makedirs(default_output_directory)
+
+    return default_output_directory
+
+
+
+# Parser arguments
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--number", type=int, dest="count", default=100, help="the number of simulations")
+parser.add_argument("--threads", type=int, dest="threads", default=4, help="the threads of simulations")
+parser.add_argument("--output", type=str, dest="output", default=get_default_output_directory(), help="the output directory for simulations")
+parser.add_argument("--invade", type=str, dest="invade", default=invade_path, help="the invade.go")
+parser.add_argument("--rep", type=int, dest="rep", default=1, help="the number of repetitions")
+parser.add_argument("--u", type=float, dest="u", default=0.2, help="the mutation rate")
+parser.add_argument("--steps", type=int, dest="steps", default=5000, help="the number of steps")
+parser.add_argument("--N", type=int, dest="N", default=1000, help="population size")
+parser.add_argument("--gen", type=int, dest="gen", default=5000, help="number of generations")
+parser.add_argument("--silent", action="store_true", dest="silent", default=False, help="be quiet; default=False")
+
+args = parser.parse_args()
+
+
 # The basic command line input for invadego which will have some parameters appended later on
 def get_basis(invade):
-    return f'{invade} -no-x-cluins --N 1000 --gen 5000 --genome mb:10,10,10,10,10 --x 0.01 --rr 4,4,4,4,4 --rep 1 --u 0.2 --steps 5000 --silent'
+    return f'{invade} -no-x-cluins --N {args.N} --gen {args.gen} --genome mb:10,10,10,10,10 --x 0.01 --rr 4,4,4,4,4 --rep {args.rep} --u {args.u} --steps {args.steps} --silent'
+
 
 # Removing irrelavant lines from putput files
 def get_filter():
@@ -67,32 +98,13 @@ def run_cluster_negsel(invade, count, output):
     for i in range(count):
         x = get_rand_clusters()
         tr = current_milli_time() + i
+        sampleid_value = x.split(',')[0]
         command_basis = get_basis(invade)
         command = f'{command_basis} --basepop "100({get_rand_bias()})" --cluster kb:{x} --replicate-offset {i} --seed {tr} '
-        command += f'--sampleid "{x}"  {get_filter()} > {os.path.join(output, str(i))}.txt'
+        command += f'--sampleid {sampleid_value} {get_filter()} > {os.path.join(output, str(i))}.txt'
         commandlist.append(command)
     return commandlist
 
-# The default directory is dynamic and depends on the time this scipt is invoked
-def get_default_output_directory():
-    current_time = time.strftime("%dth%b%yat%I%M%p", time.gmtime())
-    return os.path.join("/Users", "shashankpritam", "github", "Insertion-Bias-TE", "Simulation-Results_Files", "simulation_storm", current_time)
-
-
-# Parser arguments
-parser = argparse.ArgumentParser()
-
-parser.add_argument("--number", type=int, dest="count", default=100, help="the number of simulations")
-parser.add_argument("--threads", type=int, dest="threads", default=4, help="the threads of simulations")
-parser.add_argument("--output", type=str, dest="output", default=get_default_output_directory(), help="the output directory for simulations")
-parser.add_argument("--invade", type=str, dest="invade", default=invade_path, help="the invade.go")
-parser.add_argument("--silent", action="store_true", dest="silent", default=False, help="be quiet; default=False")
-
-args = parser.parse_args()
-
-# Create the default output directory if it doesn't exist
-if not os.path.exists(args.output):
-    os.makedirs(args.output)
 
 # Construct the command list
 commandlist = run_cluster_negsel(args.invade, args.count, args.output)
@@ -115,7 +127,7 @@ def submit_job_max_len(commandlist, max_processes):
     processes = list()
     for command in commandlist:
         if not args.silent:
-            print(f'running {len(processes)} processes. Submitting {command}')
+            print(f'Running process {len(processes)+1}. \nSubmitting {command}')
         processes.append(subprocess.Popen(command, shell=True, stdout=None))
         while len(processes) >= max_processes:
             time.sleep(sleep_time)
@@ -125,14 +137,29 @@ def submit_job_max_len(commandlist, max_processes):
         processes = [proc for proc in processes if proc.poll() is None]
 
 
+
+print(f"""Running Simulations with the following parameters:
+Number of simulations: {args.count}
+Number of threads: {args.threads}
+Output directory: {args.output}
+Invade path: {args.invade}
+Number of replications (--rep): {args.rep}
+Mutation rate (--u): {args.u}
+Number of steps (--steps): {args.steps}
+Population size (--N): {args.N}
+Number of generations (--gen): {args.gen}
+Silent mode: {args.silent}
+""")
+
+
 # This is the "main"
 submit_job_max_len(commandlist, max_processes=args.threads)
 
 
 # Cat 🐈 all the files together:
-with open(f"{get_default_output_directory()}/combined.txt", "w") as outfile:
-    for i in range(100):
-        filename = f"{get_default_output_directory()}/{i}.txt"
+with open(f"{args.output}/combined.txt", "w") as outfile:
+    for i in range(args.count):
+        filename = f"{args.output}/{i}.txt"
         subprocess.run(["cat", filename], stdout=outfile)
         
 # Sign of completion of the job.
