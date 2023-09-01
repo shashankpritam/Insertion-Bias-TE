06f4dbafa4f867d4494df8ca73849c46aa77a2ea
Shashank Pritam <shashankpritam@outlook.com>
2023-09-01 16:33:49 -0500

    ht

---
+++ Simulation-Results_Files/Slurm-Jobs/run_minfit_simulation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -N simulation_job
+#PBS -o simulation_output.log
+#PBS -e simulation_error.log
+#PBS -l nodes=1:ppn=4,mem=64gb
+#PBS -l walltime=120:00:00
+#PBS -W group_list=x-ccast-prj-[group name redacted]
+
+
+cd /mmfs1/thunder/home/shashank.pritam/shashank_simulations/invadego
+
+python3 /mmfs1/thunder/home/shashank.pritam/shashank_simulations/invadego/sim_storm.py --number 10000 --silent
